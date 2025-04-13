372942135f2e049e0544696ce5c413ae66f3c6dc
Alex Jungklaus <alexjcontato@gmail.com>
2025-04-13 13:19:42 -0300

    exercicio 12 feito

--- src/lista_05/Exercicio12.java
+++ src/lista_05/Exercicio12.java
@@ -2,9 +2,31 @@ package lista_05;
 
 public class Exercicio12 {
 
+	public static void main(String[] args) {
+
+		int[] vetorA = { 4, 5, 6, 7, 3, 10, 5 }; // 40
+
+		System.out.println(somaVetor(vetorA));
+
+	}
+
+	
+	
+	
+	public static int somaVetor(int[] valor) {
+
+		int soma = 0;
+
+		for (int i = 0; i < valor.length; i++) {
+
+			soma += valor[i];
+
+		}
+
+		return soma;
+	}
+
 }
-
-
 
 //(12) Escreva uma função para receber uma matriz A contendo números inteiros 
 //e retornar a soma dos seus elementos.
